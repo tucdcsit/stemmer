73bf428b3d782b56fafc7964cde090c6f182de37
Ashok K. Pant <ashokpant87@gmail.com>
2017-10-16 11:08:58 +0545

    Initial commit

---
+++ src/main/java/edu.cdcsit.nlp.stemmer/Stemmer.java
@@ -0,0 +1,10 @@
+package edu.cdcsit.nlp.stemmer;
+
+/**
+ * Created by Ashok K. Pant on 10/16/17.
+ */
+public class Stemmer {
+    public static void main(String[] args) {
+        System.out.println("TU Stemmer for Nepali Language");
+    }
+}
---
+++ src/test/java/edu/cdcsit/nlp/stemmer/StemmerTest.java
@@ -0,0 +1,13 @@
+package edu.cdcsit.nlp.stemmer;
+
+import org.junit.Test;
+
+/**
+ * Created by Ashok K. Pant on 10/16/17.
+ */
+public class StemmerTest {
+    @Test
+    public void stemmerTest() {
+        System.out.println("Stemmer test case");
+    }
+}
